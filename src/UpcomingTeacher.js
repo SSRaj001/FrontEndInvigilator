5ce047987b1b29ef2a0e09b6bf167dd91317432a
Rajendraprasath <59873620+SSRaj001@users.noreply.github.com>
2021-03-25 15:38:59 +0530

    made flex changes

--- src/UpcomingTeacher.js
+++ src/UpcomingTeacher.js
@@ -6,6 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Box from '@material-ui/core/Box'
 import Title from './Title';
 import RequestChange from './RequestChange'
 
@@ -59,6 +60,7 @@ export default function UpComingExams() {
           ))}
         </TableBody>
       </Table>
+      <Box flex={1}/>
       <div className={classes.extra}>
         <div className={classes.seeMore}>
             <Link color="primary" href="#" onClick={preventDefault}>
