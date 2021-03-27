1457e478929847524cbf2f44fb798f2a97789320
vishalvarma07 <kovoruvishalvarma@gmail.com>
2021-03-27 15:35:34 +0530

    update password working

--- src/Components/ChangePw.js
+++ src/Components/ChangePw.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,9 +7,41 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { auth } from "../firebase";
+import firebase from "firebase/app";
+import { UserContext } from "../providers/UserProvider";
 
 export default function ChangePw() {
   const [open, setOpen] = React.useState(false);
+  const [newPass, setNewPass] = useState('');
+  const [cnfPass, setCnfPass] = useState('');
+
+  const onChangeHandler = (event) => {
+    console.log(event.currentTarget+"here")
+    const {name, value} = event.currentTarget;
+
+    if(name === 'newPW') {
+        setNewPass(value);
+    }
+    else if(name ===  'cnfPW') {
+      setCnfPass(value);
+    }
+  };
+
+  const updatePassword = () =>{
+    var user = firebase.auth().currentUser;
+    user.updatePassword(newPass).then(() => {
+      console.log("Password updated!");
+    }).catch((error) => { console.log(error); });
+    if(newPass === cnfPass){
+      console.log(user);
+      user.updatePassword(newPass).then(() => {
+        console.log("Password updated!");
+      }).catch((error) => { console.log(error); });
+    }
+    else{
+      console.log("newPass and confirmed pass dont match");
+    }
+  }
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -35,23 +67,27 @@ export default function ChangePw() {
             id="newPW"
             label="New Password"
             type="password"
+            value = {newPass}
             name = "newPW"
             fullWidth
+            onChange = {(event) => onChangeHandler(event)}
           />
           <TextField
             margin="dense"
             id="cnfPW"
             label="Confirm Password"
             type="password"
+            value = {cnfPass}
             name = "cnfPW"
             fullWidth
+            onChange = {(event) => onChangeHandler(event)}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={updatePassword} color="primary">
             Change
           </Button>
         </DialogActions>
