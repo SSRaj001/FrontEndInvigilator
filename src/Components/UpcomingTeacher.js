5858b3fb477f39054c93c82400c1a317993210cb
vishalvarma07 <kovoruvishalvarma@gmail.com>
2021-05-19 18:25:59 +0530

    import updates

--- src/Components/UpcomingTeacher.js
+++ src/Components/UpcomingTeacher.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext,useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,7 +9,6 @@ import TableRow from '@material-ui/core/TableRow';
 import Box from '@material-ui/core/Box'
 import Title from './Title';
 import RequestChange from './RequestChange';
-import { UserContext } from "../providers/UserProvider";
 import {GetExamDetails, GetRoomLocation} from '../firebase';
 import { createMuiTheme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
