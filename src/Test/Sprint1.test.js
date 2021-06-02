866040ed2dc96f613a775937e873b3a5f2942abc
Adithi <adithi288@gmail.com>
2021-06-02 22:35:14 +0530

    Removing unwanted variables

--- src/Test/Sprint1.test.js
+++ src/Test/Sprint1.test.js
@@ -306,189 +306,169 @@ describe ('Integration Tests with Firebase Test Suite', () => {
 
 
 describe("Email_Collection Test ", () => {
+  const c = 0;
   test('Accessing Collection', async() => {
       var update = jest.fn();
       var doc = jest.fn(function () { return ({ update: update }); });
       var email_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: doc });
-      expect(email_collection).toHaveBeenCalledTimes(0);
+      expect(email_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var update = jest.fn();
       var doc = jest.fn(function () { return ({ update: update }); });
-      var email_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: doc });
-      expect(doc).toHaveBeenCalledTimes(0);
+      expect(doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var update = jest.fn();
-      var doc = jest.fn(function () { return ({ update: update }); });
-      var email_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: doc });
-      expect(update).toHaveBeenCalledTimes(0);
+      expect(update).toHaveBeenCalledTimes(c);
   });
 
 })
 describe("Rooms Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var room_update = jest.fn();
-      var room_doc = jest.fn(function () { return ({ update: update }); });
+      var room_doc = jest.fn(function () { return ({ update: room_update }); });
       var room_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: room_doc });
-      expect(room_collection).toHaveBeenCalledTimes(0);
+      expect(room_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var room_update = jest.fn();
-      var room_doc = jest.fn(function () { return ({ update: update }); });
-      var room_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: room_doc });
-      expect(room_doc).toHaveBeenCalledTimes(0);
+      var room_doc = jest.fn(function () { return ({ update: room_update }); });
+      expect(room_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var room_update = jest.fn();
-      var room_doc = jest.fn(function () { return ({ update: update }); });
-      var room_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: room_doc });
-      expect(room_update).toHaveBeenCalledTimes(0);
+      expect(room_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Classes Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var classes_update = jest.fn();
-      var classes_doc = jest.fn(function () { return ({ update: update }); });
+      var classes_doc = jest.fn(function () { return ({ update: classes_update }); });
       var classes_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: classes_doc });
-      expect(classes_collection).toHaveBeenCalledTimes(0);
+      expect(classes_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var classes_update = jest.fn();
-      var classes_doc = jest.fn(function () { return ({ update: update }); });
-      var classes_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: classes_doc });
-      expect(classes_doc).toHaveBeenCalledTimes(0);
-  });
+      var classes_doc = jest.fn(function () { return ({ update: classes_update }); });
+      expect(classes_doc).toHaveBeenCalledTimes(c);
+  })
     test('Updation', async() => {
       var classes_update = jest.fn();
-      var classes_doc = jest.fn(function () { return ({ update: update }); });
-      var classes_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: classes_doc });
-      expect(classes_update).toHaveBeenCalledTimes(0);
+      expect(classes_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Exams Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var exams_update = jest.fn();
-      var exams_doc = jest.fn(function () { return ({ update: update }); });
+      var exams_doc = jest.fn(function () { return ({ update: exams_update }); });
       var exams_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: exams_doc });
-      expect(exams_collection).toHaveBeenCalledTimes(0);
+      expect(exams_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var exams_update = jest.fn();
-      var exams_doc = jest.fn(function () { return ({ update: update }); });
-      var exams_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: exams_doc });
-      expect(exams_doc).toHaveBeenCalledTimes(0);
+      var exams_doc = jest.fn(function () { return ({ update: exams_update }); });
+      expect(exams_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var exams_update = jest.fn();
-      var exams_doc = jest.fn(function () { return ({ update: update }); });
-      var exams_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: exams_doc });
-      expect(exams_update).toHaveBeenCalledTimes(0);
+      expect(exams_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Subjects Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var subjects_update = jest.fn();
-      var subjects_doc = jest.fn(function () { return ({ update: update }); });
+      var subjects_doc = jest.fn(function () { return ({ update: subjects_update }); });
       var subjects_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: subjects_doc });
-      expect(subjects_collection).toHaveBeenCalledTimes(0);
+      expect(subjects_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var subjects_update = jest.fn();
-      var subjects_doc = jest.fn(function () { return ({ update: update }); });
-      var subjects_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: subjects_doc });
-      expect(subjects_doc).toHaveBeenCalledTimes(0);
+      var subjects_doc = jest.fn(function () { return ({ update: subjects_update }); });
+      expect(subjects_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var subjects_update = jest.fn();
-      var subjects_doc = jest.fn(function () { return ({ update: update }); });
-      var subjects_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: subjects_doc });
-      expect(subjects_update).toHaveBeenCalledTimes(0);
+      expect(subjects_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Teachers Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var teachers_update = jest.fn();
-      var teachers_doc = jest.fn(function () { return ({ update: update }); });
+      var teachers_doc = jest.fn(function () { return ({ update: teachers_update }); });
       var teachers_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: teachers_doc });
-      expect(teachers_collection).toHaveBeenCalledTimes(0);
+      expect(teachers_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var teachers_update = jest.fn();
-      var teachers_doc = jest.fn(function () { return ({ update: update }); });
-      var teachers_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: teachers_doc });
-      expect(teachers_doc).toHaveBeenCalledTimes(0);
+      var teachers_doc = jest.fn(function () { return ({ update: teachers_update }); });
+      expect(teachers_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var teachers_update = jest.fn();
-      var teachers_doc = jest.fn(function () { return ({ update: update }); });
-      var teachers_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: teachers_doc });
-      expect(teachers_update).toHaveBeenCalledTimes(0);
+      expect(teachers_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Users Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var users_update = jest.fn();
-      var users_doc = jest.fn(function () { return ({ update: update }); });
+      var users_doc = jest.fn(function () { return ({ update: users_update }); });
       var users_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: users_doc });
-      expect(users_collection).toHaveBeenCalledTimes(0);
+      expect(users_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var users_update = jest.fn();
-      var users_doc = jest.fn(function () { return ({ update: update }); });
-      var users_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: users_doc });
-      expect(users_doc).toHaveBeenCalledTimes(0);
+      var users_doc = jest.fn(function () { return ({ update: users_update }); });
+      expect(users_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var users_update = jest.fn();
-      var users_doc = jest.fn(function () { return ({ update: update }); });
-      var users_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: users_doc });
-      expect(users_update).toHaveBeenCalledTimes(0);
+      expect(users_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Requests Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var requests_update = jest.fn();
-      var requests_doc = jest.fn(function () { return ({ update: update }); });
+      var requests_doc = jest.fn(function () { return ({ update: requests_update }); });
       var requests_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requests_doc });
-      expect(requests_collection).toHaveBeenCalledTimes(0);
+      expect(requests_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var requests_update = jest.fn();
-      var requests_doc = jest.fn(function () { return ({ update: update }); });
-      var requests_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requests_doc });
-      expect(requests_doc).toHaveBeenCalledTimes(0);
+      var requests_doc = jest.fn(function () { return ({ update: requests_update }); });
+      expect(requests_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var requests_update = jest.fn();
-      var requests_doc = jest.fn(function () { return ({ update: update }); });
-      var requests_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requests_doc });
-      expect(requests_update).toHaveBeenCalledTimes(0);
+      expect(requests_update).toHaveBeenCalledTimes(c);
   });
 })
 describe("Requests History Test ", () => {
+  const c= 0;
   test('Accessing Collection', async() => {
       var requestsHistory_update = jest.fn();
-      var requestsHistory_doc = jest.fn(function () { return ({ update: update }); });
+      var requestsHistory_doc = jest.fn(function () { return ({ update: requestsHistory_update }); });
       var requestsHistory_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requestsHistory_doc });
-      expect(requestsHistory_collection).toHaveBeenCalledTimes(0);
+      expect(requestsHistory_collection).toHaveBeenCalledTimes(c);
   });
     test('Accessing Document', async() => {
       var requestsHistory_update = jest.fn();
-      var requestsHistory_doc = jest.fn(function () { return ({ update: update }); });
-      var requestsHistory_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requestsHistory_doc });
-      expect(requestsHistory_doc).toHaveBeenCalledTimes(0);
+      var requestsHistory_doc = jest.fn(function () { return ({ update: requestsHistory_update }); });
+      expect(requestsHistory_doc).toHaveBeenCalledTimes(c);
   });
     test('Updation', async() => {
       var requestsHistory_update = jest.fn();
-      var requestsHistory_doc = jest.fn(function () { return ({ update: update }); });
-      var requestsHistory_collection = jest.spyOn(admin.firestore(), 'collection').mockReturnValue({ doc: requestsHistory_doc });
-      expect(requestsHistory_update).toHaveBeenCalledTimes(0);
+      expect(requestsHistory_update).toHaveBeenCalledTimes(c);
   });
 })
 
-import firebase from "firebase/app";
 const functions = require("firebase-functions");
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
-var db = admin.firestore();
